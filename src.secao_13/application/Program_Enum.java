5a0f830edd62e7908a3990e766186ccd6d9e683a
Vinicius Santos <c.viniciussantos@outlook.com>
2021-03-05 00:17:32 -0300

    Enumerções (Enum)

---
+++ src.secao_13/application/Program_Enum.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Date;
+
+import com.sun.jmx.remote.util.OrderClassLoaders;
+
+import entities.Pedidos;
+import entities.enums.StatusPedido;
+
+public class Program_Enum {
+
+	public static void main(String[] args) {
+		
+		/*• Enumerações: É um tipo especial que serve para especificar de forma literal um
+		conjunto de constantes relacionadas
+		
+		• Palavra chave em Java: enum
+		
+		• Vantagem: melhor semântica, código mais legível e auxiliado pelo
+		compilador
+		
+		• Referência: https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html*/
+		
+		
+		Pedidos pedido = new Pedidos(125, new Date(), StatusPedido.AGUARDANDO_PAGAMENTO);
+		
+		System.out.println(pedido);
+		
+		System.out.println("-------------------------------------");
+		
+		StatusPedido st1 = StatusPedido.ENVIADO;
+		
+		StatusPedido st2 = StatusPedido.valueOf("ENVIADO");
+		
+		System.out.println(st1);
+		System.out.println(st2);
+		
+		
+	}
+
+}
