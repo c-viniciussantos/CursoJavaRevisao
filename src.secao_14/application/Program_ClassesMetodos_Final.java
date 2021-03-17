1ab7ef08e70a186e2f8a0cc38f78cc34d4011b5a
C. Vinicius Santos <c.viniciussantos@outlook.com>
2021-03-17 00:47:32 -0300

    Classes e métodos final

---
+++ src.secao_14/application/Program_ClassesMetodos_Final.java
@@ -0,0 +1,29 @@
+package application;
+
+public class Program_ClassesMetodos_Final {
+
+	public static void main(String[] args) {
+
+		 /*Palavra chave: final
+		 • Classe: evita que a classe seja herdada
+		 public final class ContaPoupanca {
+		 
+		 • Método: evita que o método sob seja sobreposto
+		 Suponha que você queira evitar que sejam criadas subclasses de ContaPoupanca
+		 public final void saque()*/
+		
+		//Pra quê?
+		
+		/*• Segurança: dependendo das regras do negócio, às vezes é desejável
+		garantir que uma classe não seja herdada, ou que um método não
+		seja sobreposto.
+		• Geralmente convém acrescentar final em métodos sobrepostos, pois
+		sobreposições múltiplas podem ser uma porta de entrada para
+		inconsistências
+		• Performance: atributos de tipo de uma classe final são analisados de
+		forma mais rápida em tempo de execução.
+		• Exemplo clássico: String*/
+
+	}
+
+}
