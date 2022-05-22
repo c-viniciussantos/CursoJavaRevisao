d3221049494b4afb17c808afa1a1a760198571d9
Vinicius Santos <c.viniciussantos@outlook.com>
2022-05-22 04:06:27 -0300

    Exe Fixacao: Add ProdutoUsado

---
+++ src.secao_14/entities/ProdutoUsado.java
@@ -0,0 +1,34 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ProdutoUsado extends Produto_ExeFixacao{
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	private Date dataFabricacao;
+
+	public ProdutoUsado(String nome, Double preco, Date dataFabricacao) {
+		super(nome, preco);
+		this.dataFabricacao = dataFabricacao;
+	}
+
+	public Date getDataFabricacao() {
+		return dataFabricacao;
+	}
+
+	public void setDataFabricacao(Date dataFabricacao) {
+		this.dataFabricacao = dataFabricacao;
+	}
+	
+	@Override
+	public String precoTag() {
+		return getNome() 
+				+ " (used) $ " 
+				+ String.format("%.2f", getPreco())
+				+ " (Manufacture date: "
+				+ sdf.format(dataFabricacao)
+				+ ")";
+	}
+}
